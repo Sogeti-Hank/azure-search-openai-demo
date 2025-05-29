731a7f8e592fe98ca6ace36194fe05a6ccd1bf5f
Sogeti-Hank <120056452+Sogeti-Hank@users.noreply.github.com>
2025-05-29 15:36:17 +0000

    Hard coded 2025-IFBAPSCPCMN87 into query filter builder under approach as a test

--- app/backend/approaches/approach.py
+++ app/backend/approaches/approach.py
@@ -184,7 +184,7 @@ class Approach(ABC):
         if security_filter:
             filters.append(security_filter)
 
-        filters.append("planid eq ''2025-IFBAPSCPCMN87''")    ## Hank  Hardcoded as a test for now
+        filters.append("planid eq '2025-IFBAPSCPCMN87'")    ## Hank  Hardcoded as a test for now
         return None if len(filters) == 0 else " and ".join(filters)
 
     async def search(
